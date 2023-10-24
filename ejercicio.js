1445787980b24a70daa5f1bd5f5de84d4219e4fc
lexymolina <lexymato@colegioguanenta.edu.co>
2023-10-24 09:25:22 -0500

    PROYECTO_FINAL

--- ejercicio.js
+++
@@ -1,5 +0,0 @@
-// Ejemplo de JavaScript para agregar interacción
-document.addEventListener("DOMContentLoaded", function () {
-    // Tu código JavaScript aquí
-    // Puedes agregar funciones para interactuar con la base de datos o formularios
-});
